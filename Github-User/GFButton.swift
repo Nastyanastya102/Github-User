cce778252f4ac12f88e6029f30de2d0c7636284f
StacyKlyashtorna <Little.smart.bee@gmail.com>
2024-11-06 21:21:52 -0800

    Add custome button & text field

---
+++ Github-User/GFButton.swift
@@ -0,0 +1,35 @@
+//
+//  GFButton.swift
+//  Github-User
+//
+//  Created by Nastya Klyashtorna on 2024-11-06.
+//
+
+import UIKit
+
+class GFButton: UIButton {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(backgroundColor: UIColor, title: String) {
+        super.init(frame: .zero)
+        self.backgroundColor = backgroundColor
+        self.setTitle(title, for: .normal)
+        configure()
+    }
+    
+    private func configure () {
+
+        layer.cornerRadius = 10
+        titleLabel?.textColor = .white
+        titleLabel?.font = UIFont.preferredFont(forTextStyle: .headline)
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+}
---
+++ Github-User/GFTextField.swift
@@ -0,0 +1,35 @@
+//
+//  GFTextField.swift
+//  Github-User
+//
+//  Created by Nastya Klyashtorna on 2024-11-06.
+//
+
+import UIKit
+
+class GFTextField: UITextField {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setup() {
+        translatesAutoresizingMaskIntoConstraints = false
+        layer.cornerRadius = 10
+        layer.borderWidth = 2
+        layer.borderColor = UIColor.systemGray4.cgColor
+        textColor = .label
+        tintColor = .label
+        textAlignment = .center
+        font = .preferredFont(forTextStyle: .title2)
+        adjustsFontSizeToFitWidth = true
+        minimumFontSize = 12
+        backgroundColor = .tertiarySystemBackground
+        autocorrectionType = .no
+        placeholder = "Enter a user name"
+    }
+}
