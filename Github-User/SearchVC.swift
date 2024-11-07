eae8387713da7c0208347b8162c432009358c00b
StacyKlyashtorna <Little.smart.bee@gmail.com>
2024-11-06 21:52:56 -0800

    Search screen UI

--- Github-User/SearchVC.swift
+++ Github-User/SearchVC.swift
@@ -8,9 +8,59 @@
 import UIKit
 
 class SearchVC: UIViewController {
-
+    let logoImageView = UIImageView()
+    let userNameTextFileld = GFTextField()
+    let searchButton = GFButton(backgroundColor: .green, title: "Get followers")
+    let activityIndicator = UIActivityIndicatorView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        // Why here it's view and in button and text field it's layer?
+        view.backgroundColor = .systemBackground
+        configureLogoImageView()
+        configureTextField()
+        configureButton()
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.isNavigationBarHidden = true
+    }
+    
+    func configureLogoImageView () {
+        logoImageView.image = UIImage(named: "gh-logo")
+        logoImageView.contentMode = .scaleAspectFit
+        logoImageView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(logoImageView)
+        
+        NSLayoutConstraint.activate([
+            logoImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 80),
+            logoImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            logoImageView.heightAnchor.constraint(equalToConstant: 200),
+            logoImageView.widthAnchor.constraint(equalToConstant: 200),
+        ])
+    }
+    
+    func configureTextField() {
+        view.addSubview(userNameTextFileld)
+        
+        NSLayoutConstraint.activate([
+            userNameTextFileld.topAnchor.constraint(equalTo: logoImageView.bottomAnchor, constant: 48),
+            userNameTextFileld.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 50),
+            userNameTextFileld.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -50),
+            userNameTextFileld.heightAnchor.constraint(equalToConstant: 50),
+        ])
+    }
+    
+    func configureButton() {
+        view.addSubview(searchButton)
+        
+        NSLayoutConstraint.activate([
+            searchButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -50),
+            searchButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 50),
+            searchButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -50),
+            searchButton.heightAnchor.constraint(equalToConstant: 50),
+            ])
+       }
 
 }
