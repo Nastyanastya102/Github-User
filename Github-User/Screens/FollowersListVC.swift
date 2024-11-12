b958e9abe7ff5c85f94fe12bce5790b6b85f2734
StacyKlyashtorna <Little.smart.bee@gmail.com>
2024-11-11 20:04:55 -0800

    UICollectionView - FlowLayout & Diffable Data Source

--- Github-User/Screens/FollowersListVC.swift
+++ Github-User/Screens/FollowersListVC.swift
@@ -2,18 +2,24 @@
 import UIKit
 
 class FollowersListVC: UIViewController {
+    enum Section {
+        case main
+    }
+    
     var userName: String = ""
     var followers: [Follower] = []
     let tableView = UITableView()
     var collectionView: UICollectionView! = nil
+    var dataSource: UICollectionViewDiffableDataSource<Section, Follower>!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         configureViewController()
         getFollowers()
+        configureCollectionView()
+        configureDataSource()
 //        setupTableView()
 
-       
     }
     
     func getFollowers() {
@@ -21,9 +27,8 @@ class FollowersListVC: UIViewController {
             switch(result) {
             case .success(let flw):
                 self.followers = flw
-                DispatchQueue.main.async {
-                    self.tableView.reloadData()
-                }
+                self.updateData()
+
                 break
             case .failure(let error):
                 switch error {
@@ -47,7 +52,6 @@ class FollowersListVC: UIViewController {
     func configureViewController() {
         view.backgroundColor = .systemBackground
         navigationController?.navigationBar.prefersLargeTitles = true
-        
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -70,33 +74,63 @@ extension FollowersListVC: UITableViewDelegate, UITableViewDataSource {
         return cell
     }
     
-    // MARK: - UITableViewDelegate
-
+    func createThereCollectionFlowLayout() -> UICollectionViewFlowLayout {
+        let width = view.bounds.width
+        let padding: CGFloat = 12
+        let minimumItemSpacing: CGFloat = 10
+        let avaiableWidth = width - (padding * 2) - (minimumItemSpacing * 2)
+        let itemWidth = avaiableWidth / 3
+        
+        let layout = UICollectionViewFlowLayout()
+        layout.sectionInset = UIEdgeInsets(top: padding, left: padding, bottom: padding, right: padding)
+        layout.itemSize = CGSize(width: itemWidth, height: itemWidth + 40)
+        
+        return layout
+    }
+    
       func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
           print("Selected item: \(followers[indexPath.row])")
       }
     
     func setupTableView() {
-           view.addSubview(tableView)
+        view.addSubview(tableView)
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.register(GFFollowerCellTableViewCell.self, forCellReuseIdentifier: "GFFollowerCellTableViewCell")
 
-           tableView.translatesAutoresizingMaskIntoConstraints = false
-           NSLayoutConstraint.activate([
-               tableView.topAnchor.constraint(equalTo: view.topAnchor),
-               tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
-               tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-               tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
-           ])
-
-           tableView.delegate = self
-           tableView.dataSource = self
-
-           tableView.register(GFFollowerCellTableViewCell.self, forCellReuseIdentifier: "GFFollowerCellTableViewCell")
-       }
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.topAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+        ])
+        
+    }
     
     func configureCollectionView() {
-        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: UICollectionViewFlowLayout())
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: createThereCollectionFlowLayout())
         view.addSubview(collectionView)
         collectionView.backgroundColor = .white
         collectionView.register(GFFollowerCell.self, forCellWithReuseIdentifier: GFFollowerCell.identifier)
+    }
+    
+    func configureDataSource() {
+        dataSource = UICollectionViewDiffableDataSource<Section, Follower>(collectionView: collectionView) { collectionView, indexPath, follower in
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: GFFollowerCell.identifier, for: indexPath) as! GFFollowerCell
+            cell.set(follower: follower)
+            return cell
+        }
+    }
+    
+    func updateData() {
+        var snapshot = NSDiffableDataSourceSnapshot<Section, Follower>()
+        snapshot.appendSections([.main])
+        snapshot.appendItems(followers)
+        DispatchQueue.main.async {
+            self.dataSource.apply(snapshot, animatingDifferences: true)
+        }
+       
     }
 }
