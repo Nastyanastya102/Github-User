ccb150132456a1514b8a5784bf5d694376cd65a8
StacyKlyashtorna <Little.smart.bee@gmail.com>
2024-11-17 20:00:48 -0800

    UserInfoVC Cleanup & Setup

--- Github-User/Screens/UserInfoVC.swift
+++ Github-User/Screens/UserInfoVC.swift
@@ -11,13 +11,19 @@ class UserInfoVC: UIViewController {
     var username: String!
 
     let header = UIView()
+    let itemViewOne = UIView()
+    let itemViewTwo = UIView()
+    
+    var itemView: [UIView] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
         layoutUI()
-        view.backgroundColor = .systemBackground
-        let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(dusmissVC))
-        navigationItem.rightBarButtonItem = doneButton
+        configureViewController()
+        getUserInfo()
+    }
+    
+    func getUserInfo() {
         NetworkManager.shared.getUser(for: username, completed: { [weak self] result in
             guard let self = self else { return }
             print(result)
@@ -26,9 +32,17 @@ class UserInfoVC: UIViewController {
                     self.addChildVC(childVC: GFUserInfoHeaderVC(user: user), to: self.header)
                 }
             }
+            if case .failure(let error) = result {
+                self.presentGFAlertOnMainThred(title: "Something went wrong", message: error.localizedDescription , buttonTitle: "OK")
+            }
             
         })
-
+    }
+    
+    func configureViewController() {
+        view.backgroundColor = .systemBackground
+        let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(dusmissVC))
+        navigationItem.rightBarButtonItem = doneButton
     }
     
     @objc func dusmissVC() {
@@ -36,22 +50,39 @@ class UserInfoVC: UIViewController {
     }
     
     func layoutUI () {
-        view.addSubview(header)
-        header.translatesAutoresizingMaskIntoConstraints = false
+        let padding: CGFloat = 20
         
+        itemView = [header, itemViewOne, itemViewTwo]
+        
+        for item in itemView {
+            view.addSubview(item)
+            item.translatesAutoresizingMaskIntoConstraints = false
+        }
+
         NSLayoutConstraint.activate([
             header.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
             header.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             header.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            header.heightAnchor.constraint(equalToConstant: 180)
+            header.heightAnchor.constraint(equalToConstant: 180),
+            
+            itemViewOne.topAnchor.constraint(equalTo: header.bottomAnchor, constant: padding),
+            itemViewOne.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: padding),
+            itemViewOne.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -padding),
+            itemViewOne.heightAnchor.constraint(equalToConstant: 140),
+            
+            itemViewTwo.topAnchor.constraint(equalTo: itemViewOne.bottomAnchor, constant: padding),
+            itemViewTwo.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: padding),
+            itemViewTwo.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -padding),
+            itemViewTwo.heightAnchor.constraint(equalToConstant: 140),
         ])
     }
     
     func addChildVC (childVC: UIViewController, to container: UIView) {
-        print("HERE")
         addChild(childVC)
         container.addSubview(childVC.view)
         childVC.view.frame = container.bounds
         childVC.didMove(toParent: self)
+        
+        // @TODO: get deeper into topic
     }
 }
